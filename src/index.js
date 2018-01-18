909c82fe6178a21350c12a98a0bc9f39c1140f31
Tyler Adams <tyleraadams@gmail.com>
2018-01-17 20:56:40 -0500

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import './utils.css';
-import App from './app/app';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
